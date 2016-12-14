2d05a339b628817863e56b02dbc8f2f152d68e01
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-14 14:18:42 -0700

    added mock database test (fails)

---
+++ test/database.test.js
@@ -0,0 +1,14 @@
+
+
+describe('read from database', () => {
+  it('should return true if data is read', () => {
+    true.should.eql(false)
+  })
+})
+
+describe('write to database', () => {
+  it('should return true if data is written', () => {
+    true.should.eql(false)
+  })
+})
+
